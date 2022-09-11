99e316adf90a6e560d09dc9c2a850dcf8990e33e
Ratchapon <ratchapon.sang@kmutt.ac.th>
2022-09-11 18:05:29 +0700

    sent Hw02-2

--- totalPages.js
+++ totalPages.js
@@ -1,6 +1,20 @@
 const { template } = require('@babel/core')
 
 function totalPages(arrayItems, rowsPerPage) {
-  //code here
+  if(rowsPerPage == null || undefined || 0){
+    // console.log(1)
+    return 1
+  }
+  else if(arrayItems == null || undefined){
+    // console.log(undefined)
+    return undefined
+  }
+  else{
+    page = arrayItems.length/rowsPerPage
+    // console.log(page)
+    return Math.round(page)
+    
+  }
+
 }
 module.exports = totalPages
